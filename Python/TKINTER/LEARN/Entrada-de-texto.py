e294b990219d92a419eb3e9b58b81c04c295a2c3
CMD <exodojaffar@gmail.com>
2019-02-05 17:15:22 -0300

    Atualizando codigo para verso recente que eu sei

--- Python/TKINTER/LEARN/Entrada-de-texto.py
+++ Python/TKINTER/LEARN/Entrada-de-texto.py
@@ -1,18 +1,30 @@
 #-*-coding:utf8;-*-
 #py:3.6
+#tkinter:8.6.6
 
 from tkinter import *
+from tkinter import ttk
 
-root = Tk()
-soma = DoubleVar(root)
-parcela = DoubleVar(root)
+# Functions : 
 def aritmetica(e):
 	soma.set(soma.get()+parcela.get())
 
-lsoma= Label(textvar=soma)
-entrada = Entry(textvar=parcela)
-entrada.bind("<Return>", aritmetica)
-lsoma.pack()
-entrada.pack(side='bottom')
-mainloop()
+# Criando widget principal : 
+root = Tk()
+
+# Criando variaveis :
+soma = DoubleVar() 
+parcela = DoubleVar()
+
+# Criando widgets : 
+lsoma= Label(root, textvar=soma)
+entrada = Entry(root, textvar=parcela)
+entrada.bind('<Return>', aritmetica) # ????
+
+# Iniciando widgets com grid :
+lsoma.grid()
+entrada.grid()
+
+# Iniciando widget principal : 
+root.mainloop()
 
